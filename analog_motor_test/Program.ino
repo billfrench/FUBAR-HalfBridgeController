22f44251066dfe23fdef28a74624a8c1abadcf79
BillFrench <william.french@gmail.com>
2017-08-01 22:19:05 -0600

    simplified the over amp cutout code and cleaned up / added some comments

--- analog_motor_test/Program.ino
+++ analog_motor_test/Program.ino
@@ -17,27 +17,27 @@ void loop() {
   FUSE_SHUNT_READING = analogRead(FUSE_IN_PIN);
   if (FUSE_SHUNT_READING > AMP_CUTOUT)
   {
-    OUTPUT_PWM = CUTOUT_PWM;
-    Timer1.pwm(LOW_SIDE_OUT, OUTPUT_PWM);  // Fuse is over amps, set PWM to zero
+    Timer1.pwm(LOW_SIDE_OUT, CUTOUT_PWM);  // Fuse is over amps, set PWM to CUTOUT_PWM (used to be zero, but now a definable constant, to help keep the motor energized a bit.
   }
   else
   {
-    
+
     int ThrottleReading = analogRead(THROTTLE_IN_PIN);
 
     // the timerone library for some reason needs 0 to 1024 ... not 1023!
-    ThrottleReading = map(ThrottleReading, 200, 800, 0, 1024);
+    // issue filed: https://github.com/PaulStoffregen/TimerOne/issues/19
+    ThrottleReading = map(ThrottleReading, 200, 800, 0, 1024);  //Throttle reads from about 200-800 for zero to full throttle
     ThrottleReading = constrain(ThrottleReading, 0, 1024);
 
     OUTPUT_PWM = ThrottleLUT[ThrottleReading];  // this lookup table allows for an exponential throttle curve
     Timer1.pwm(LOW_SIDE_OUT, OUTPUT_PWM);
-    
+
   }
-  //    Serial.print(T);
-  //    Serial.print(",");
-//  Serial.print(FUSE_SHUNT_READING);
-//  Serial.print(",");
-//  Serial.print(OUTPUT_PWM);
-//  Serial.print("X");
+  //  Serial.print(T);
+  //  Serial.print(",");
+  //  Serial.print(FUSE_SHUNT_READING);
+  //  Serial.print(",");
+  //  Serial.print(OUTPUT_PWM);
+  //  Serial.print("X");
 }
 
