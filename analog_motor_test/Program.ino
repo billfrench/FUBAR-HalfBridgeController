b31edfe0089c0394913052c808ca6e97a6ca9a49
Bill.French <william.french@gmail.com>
2018-06-29 07:47:43 -0600

    Had to switch current direction with new circuit board.

--- analog_motor_test/Program.ino
+++ analog_motor_test/Program.ino
@@ -12,17 +12,20 @@ void setup() {
   Serial.println(AMP_CUTOUT);
 
 }
+
+int ThrottleReading = 0;
 void loop() {
 
   FUSE_SHUNT_READING = analogRead(FUSE_IN_PIN);
-  if (FUSE_SHUNT_READING > AMP_CUTOUT)
+  if (FUSE_SHUNT_READING < AMP_CUTOUT)
   {
     Timer1.pwm(LOW_SIDE_OUT, CUTOUT_PWM);  // Fuse is over amps, set PWM to CUTOUT_PWM (used to be zero, but now a definable constant, to help keep the motor energized a bit.
+    
   }
   else
   {
 
-    int ThrottleReading = analogRead(THROTTLE_IN_PIN);
+    ThrottleReading = analogRead(THROTTLE_IN_PIN);
 
     // the timerone library for some reason needs 0 to 1024 ... not 1023!
     // issue filed: https://github.com/PaulStoffregen/TimerOne/issues/19
@@ -33,11 +36,15 @@ void loop() {
     Timer1.pwm(LOW_SIDE_OUT, OUTPUT_PWM);
 
   }
-  //  Serial.print(T);
-  //  Serial.print(",");
-  //  Serial.print(FUSE_SHUNT_READING);
-  //  Serial.print(",");
-  //  Serial.print(OUTPUT_PWM);
-  //  Serial.print("X");
+////    Serial.print(ThrottleReading);
+////    Serial.print(",");
+// Serial.print(FUSE_SHUNT_READING);
+//   Serial.print(",");
+//    Serial.println(OUTPUT_PWM);
+//    if (OUTPUT_PWM == 50)
+//    {
+//      Serial.println("********");
+//    }
+////    Serial.print("X");
 }
 
